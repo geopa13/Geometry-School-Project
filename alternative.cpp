6aff69497104c0fd860f34ddd00c91557a636fb8
theowner997 <georgelaf997@hotmail.com>
2014-01-27 17:40:11 +0200

    Update alternative.cpp

--- alternative.cpp
+++ alternative.cpp
@@ -18,10 +18,19 @@ int main
     cout << "4: Circle" << endl;
     Sleep(200);
     cout << "5: Triangle" << endl;
+    cout << " " << endl;
+    Sleep(100);
+    cout <<"Other Options" << endl;
     Sleep(200);
-    cout << "6: 
-    
-    
+    cout << "6: About Developers/Coders" << endl;
+    Sleep(200);
+    cout << "7: Thanks Section" << endl;
+    Sleep(200);
+    cout << "8: Exit" << endl;
+    cout << " " << endl;
+    Sleep(100);
+    cout << "Type the number of the option you want";
+    cout << " and press ENTER" << endl;
     
     
     
