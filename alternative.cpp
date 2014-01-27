5dffba88fd745178847ea055ccb5c25ec8e255b0
theowner997 <georgelaf997@hotmail.com>
2014-01-27 17:34:51 +0200

    Create alternative.cpp

---
+++ alternative.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+
+int main 
+{
+    using namespace std;
+    /* MAIN PROGRAM */
+    system("cls");
+    cout << "Geometric Shapes Options" << endl;
+    Sleep(200);
+    cout << "1: Straight Line" << endl;
+    Sleep(200);
+    cout << "2: Square" << endl;
+    Sleep(200);
+    cout << "3: Rectangle" << endl;
+    Sleep(200);
+    cout << "4: Circle" << endl;
+    Sleep(200);
+    cout << "5: Triangle" << endl;
+    Sleep(200);
+    cout << "6: 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    system("pause");
+    return 0;
+    }
